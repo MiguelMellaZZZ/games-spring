688081f47621aeadd8d631d25b734dc4ffd956fd
N1ghtwZ ZZ <miguelmellasilva123@gmail.com>
2025-02-17 17:57:51 +0000

    Categorias LIST

--- app/src/main/java/application/model/Categoria.java
+++ app/src/main/java/application/model/Categoria.java
@@ -12,15 +12,40 @@ import jakarta.persistence.OneToMany;
 
 @Entity
 @Table(name = "categorias")
-@
+
 
 public class Categoria{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(unique=true, nullable=false)
-    
     private String nome;
 
+    @OneToMany (mappedBy = "categoria")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
     
-} 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Jogo> getJogos () {
+        return jogos;
+    }
+    
+    public void set Jogos (Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+
+}
