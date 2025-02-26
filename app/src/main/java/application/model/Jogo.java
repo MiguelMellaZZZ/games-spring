f4346d3ddb546d8363d438837a54ee214a26fc2f
N1ghtwZ ZZ <miguelmellasilva123@gmail.com>
2025-02-26 19:30:32 +0000

    Pag 42

--- app/src/main/java/application/model/Jogo.java
+++ app/src/main/java/application/model/Jogo.java
@@ -1,32 +1,32 @@
 package application.model;
 import application.model.Plataforma;
 
-import java.util.HashSet; 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity; 
-import jakarta.persistence.GeneratedValue; 
-import jakarta.persistence.GenerationType; 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn; 
-import jakarta.persistence.JoinTable; 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne; 
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "jogos")
 public class Jogo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(nullable = true)
     private String titulo;
 
     @ManyToOne
     @JoinColumn(name = "id_genero")
-    private Categoria categoria;    
+    private Categoria categoria;
 
     @ManyToMany
     @JoinTable(
@@ -47,7 +47,7 @@ public class Jogo {
         return titulo;
     }
     
-    public void setTitulo(String titulo) { 
+    public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
 
@@ -59,7 +59,7 @@ public class Jogo {
         this.categoria = categoria;
     }
     
-    public Set<Plataforma> getPlataformas () { 
+    public Set<Plataforma> getPlataformas () {
         return plataformas;
     }
     public void setPlataformas (Set<Plataforma> plataformas) {
---
+++ app/src/main/java/application/repository/PlataformaRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.Plataforma;
+
+public interface CategoriaRepository extends CrudRepository<Plataforma, Long> {
+
+}
